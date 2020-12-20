3783f86e5d10cdc030ec93dbb8dca2461fba69b2
Bill <bill.zielinski@gmail.com>
2020-12-19 23:53:08 -0600

    On S3 trigger, save speed data to DynamoDB

--- amplify/backend/function/S3Triggercd49230a/src/index.js
+++ amplify/backend/function/S3Triggercd49230a/src/index.js
@@ -1,38 +1,76 @@
+/* Amplify Params - DO NOT EDIT
+	API_SPEEDDATAAPI_GRAPHQLAPIIDOUTPUT
+	API_SPEEDDATAAPI_VEHICLETABLE_ARN
+	API_SPEEDDATAAPI_VEHICLETABLE_NAME
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
 // const { findByPlaceholderText } = require("@testing-library/react");
 
 // Load the AWS SDK for Node.js
-import { DynamoDB } from 'aws-sdk';
+const AWS = require('aws-sdk');
 
-function createVehicleSpeedData(imageFilename, date, speed, direction, vehicleType) {
-  console.log('createVehicleSpeedData:', date.toISOString(), speed, direction, vehicleType);
+const extractVehicleSpeedDataFromImageFilename = (imageFilename) => {
+    const [timeString, speed, directionCode, vehicleTypeCode] = imageFilename.split('_');
+    const [year, month, day, hour, minute] = timeString.split('-');
+    const date = new Date(year, month-1, day, hour, minute);
+
+    const direction = {
+      'N': 'north',
+      'S': 'south'
+    }[directionCode];
+
+    const vehicleType = {
+      'V': '',
+      'B': 'bus',
+      'P': 'police',
+      'F': 'fire',
+      'D': 'delivery'
+    }[vehicleTypeCode];
+
+    return {
+      id: imageFilename,
+      date,
+      speed,
+      direction,
+      vehicleType
+    };
+}
+
+const storeVehicleSpeedData = async (vehicleSpeedData) => {
+  console.log('createVehicleSpeedData:', JSON.stringify(vehicleSpeedData));
 
   // Create the DynamoDB service object
-  const ddb = new DynamoDB({apiVersion: '2012-08-10'});
+  const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
   
+  const {id, date, speed, direction, vehicleType} = vehicleSpeedData;
+
   var params = {
     TableName: 'Vehicle-pgvfaidzgzfdhkhg5mjgdvaheu-dev',
     Item: {
-      'id': {S: imageFilename},
+      'id': {S: id},
       'time' : {S: date.toISOString()},
       'speed': {N: speed},
       'direction': {S: direction},
       'vehicle': {S: vehicleType}
     },
   };
-  
-  // Add the items to the DynamoDB table
-  ddb.putItem(params, function(err, data) {
-    if (err) {
-      console.log('Error: ', err);
-    }
-    else {
-      console.log('Vehicle item entered successfully:', data);
-    }
-  });
+
+  // Add the item to the DynamoDB table
+  try {
+    console.log('calling ddb.putItem', params);
+    const result = await ddb.putItem(params).promise();
+
+    console.log('ddb.putItem Success:', result);
+  }
+  catch (err) {
+    console.log('ddb.putItem Error: ', err);
+  }
 }
 
 // eslint-disable-next-line
-export function handler(event, context) {
+exports.handler = async (event, context) => {
   console.log('Received S3 event:', JSON.stringify(event, null, 2));
 
   const bucket = event.Records[0].s3.bucket.name;
@@ -48,29 +86,12 @@ export function handler(event, context) {
     // A new image was added to the S3 bucket.
     
     // Extract data values from image filename.
-    const [timeString, speedString, directionCode, vehicleTypeCode] = imageFilename.split('_');
-    const [year, month, day, hour, minute] = timeString.split('%3A');
-    const date = new Date(year, month-1, day, hour, minute);
-    const speed = +speedString; // convert to number
-
-    const direction = {
-      'N': 'north',
-      'S': 'south'
-    }[directionCode];
-
-    const vehicleType = {
-      'V': '',
-      'B': 'bus',
-      'P': 'police',
-      'F': 'fire',
-      'D': 'delivery'
-    }[vehicleTypeCode];
+    const vehicleSpeedData = extractVehicleSpeedDataFromImageFilename(imageFilename);
 
     // Add data to vehicle speed table
-    createVehicleSpeedData(imageFilename, date, speed, direction, vehicleType);
+    await storeVehicleSpeedData(vehicleSpeedData);
   }
 
-  console.log(`Bucket: ${bucket}`, `Key: ${key}`);
   context.done(null, 'Successfully processed S3 event'); // SUCCESS with message
 }
 
--- src/App.js
+++ src/App.js
@@ -1,67 +1,86 @@
 /* src/App.js */
 import React, { useEffect, useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
-import { createTodo } from './graphql/mutations'
-import { listTodos } from './graphql/queries'
+import { createVehicle } from './graphql/mutations'
+import { listVehicles } from './graphql/queries'
 
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
-const initialState = { name: '', description: '' }
+const initialState = {
+  id: 'test id 1',
+  time: new Date('2020-12-13T14:29:53.000Z'),
+  speed: 22.0,
+  direction: 'north'
+};
 
 const App = () => {
   const [formState, setFormState] = useState(initialState)
-  const [todos, setTodos] = useState([])
+  const [vehicles, setVehicles] = useState([])
 
   useEffect(() => {
-    fetchTodos()
+    fetchVehicles()
   }, [])
 
   function setInput(key, value) {
     setFormState({ ...formState, [key]: value })
   }
 
-  async function fetchTodos() {
+  async function fetchVehicles() {
     try {
-      const todoData = await API.graphql(graphqlOperation(listTodos))
-      const todos = todoData.data.listTodos.items
-      setTodos(todos)
-    } catch (err) { console.log('error fetching todos') }
+      const vehicleData = await API.graphql(graphqlOperation(listVehicles))
+      const vehicles = vehicleData.data.listVehicles.items
+      setVehicles(vehicles)
+    } catch (err) { console.log('error fetching vehicles') }
   }
 
-  async function addTodo() {
+  async function addVehicle() {
     try {
-      if (!formState.name || !formState.description) return
-      const todo = { ...formState }
-      setTodos([...todos, todo])
+      if (!formState.id || !formState.time || !formState.speed || !formState.direction) return
+      const vehicle = { ...formState }
+      setVehicles([...vehicles, vehicle])
       setFormState(initialState)
-      await API.graphql(graphqlOperation(createTodo, {input: todo}))
+      await API.graphql(graphqlOperation(createVehicle, {input: vehicle}))
     } catch (err) {
-      console.log('error creating todo:', err)
+      console.log('error creating vehicle:', err)
     }
   }
 
   return (
     <div style={styles.container}>
-      <h2>Amplify Todos</h2>
+      <h2>Amplify Vehicles</h2>
       <input
-        onChange={event => setInput('name', event.target.value)}
+        onChange={event => setInput('id', event.target.value)}
         style={styles.input}
-        value={formState.name}
-        placeholder="Name"
+        value={formState.id}
+        placeholder="Id"
+      />
+      {/* <input
+        onChange={event => setInput('time', new Date(event.target.value))}
+        style={styles.input}
+        value={formState.time}
+        placeholder="Time"
+      /> */}
+      <input
+        onChange={event => setInput('speed', event.target.value)}
+        style={styles.input}
+        value={formState.speed}
+        placeholder="Speed"
       />
       <input
-        onChange={event => setInput('description', event.target.value)}
+        onChange={event => setInput('direction', event.target.value)}
         style={styles.input}
-        value={formState.description}
-        placeholder="Description"
+        value={formState.direction}
+        placeholder="Direction"
       />
-      <button style={styles.button} onClick={addTodo}>Create Todo</button>
+      <button style={styles.button} onClick={addVehicle}>Create Vehicle</button>
       {
-        todos.map((todo, index) => (
-          <div key={todo.id ? todo.id : index} style={styles.todo}>
-            <p style={styles.todoName}>{todo.name}</p>
-            <p style={styles.todoDescription}>{todo.description}</p>
+        vehicles.map((vehicle, index) => (
+          <div key={vehicle.id ? vehicle.id : index} style={styles.vehicle}>
+            <p style={styles.vehicleId}>{vehicle.id}</p>
+            {/* <p style={styles.vehicleTime}>{vehicle.time.toISOString()}</p> */}
+            <p style={styles.vehicleSpeed}>{vehicle.speed}</p>
+            <p style={styles.vehicleDirection}>{vehicle.direction}</p>
           </div>
         ))
       }
@@ -71,10 +90,12 @@ const App = () => {
 
 const styles = {
   container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
+  vehicle: {  marginBottom: 15 },
   input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
+  vehicleId: { fontSize: 20, fontWeight: 'bold' },
+  vehicleTime: { marginBottom: 0 },
+  vehicleSpeed: { marginBottom: 0 },
+  vehicleDirection: { marginBottom: 0 },
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
